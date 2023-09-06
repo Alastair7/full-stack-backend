872f35acb5367cf94089db6ae583e45a250f141d
alastair7 <alexandru.ban.drmer@gmail.com>
2023-09-06 22:42:55 +0200

    Part 3 Exercise 3.20 Phonebook

--- models/person.js
+++ models/person.js
@@ -17,7 +17,16 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return /\d{2,3}-\d*$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid number.`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
